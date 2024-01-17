a70513d986f97a194dbf282b479f0e261219e279
laksanautama <kussa_laksana@uhnsugriwa.ac.id>
2024-01-17 09:58:43 +0800

    submit file cpp and pdf

---
+++ rencana_liburan.cpp
@@ -0,0 +1,20 @@
+#include <map>
+#include <set>
+#include <string>
+
+using namespace std;
+
+//----Tidak boleh menambah kode di atas batas ini-----
+
+map<int, string> nama_obyek;
+
+void kombinasi_obyek_wisata(){
+
+
+}
+
+
+int main(){
+
+return 0;
+}
